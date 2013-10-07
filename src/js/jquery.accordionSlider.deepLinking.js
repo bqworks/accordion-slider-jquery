a9ebb41654fc3244cadb9bd4db2ca101d95022b7
davidghi <david.ghiurau@gmail.com>
2013-10-07 13:32:17 +0300

    ignore startPanel if there is an initial hash

--- src/js/jquery.accordionSlider.deepLinking.js
+++ src/js/jquery.accordionSlider.deepLinking.js
@@ -14,14 +14,18 @@
 		initDeepLinking: function() {
 			var that = this;
 
+			// ignore the startPanel setting if there is a 
+			if (this._parseHash(window.location.hash) !== false)
+				this.options.startPanel = -1;
+
 			// parse the initial hash
 			this.on('init.DeepLinking.' + NS, function() {
-				that._parseHash(window.location.hash);
+				that._gotoHash(window.location.hash);
 			});
-			
+
 			// check when the hash changes
 			$(window).on('hashchange.DeepLinking.' + this.uniqueId + '.' + NS, function() {
-				that._parseHash(window.location.hash);
+				that._gotoHash(window.location.hash);
 			});
 		},
 
@@ -35,22 +39,33 @@
 					panelId = values.pop(),
 					accordionId = hash.slice(0, - panelId.toString().length - 1);
 
-				if (this.$accordion.attr('id') == accordionId) {
-					var panelIdNumber = parseInt(panelId, 10);
-
-					// check if the specified panel id is a number or string
-					if (isNaN(panelIdNumber)) {
-						// get the index of the panel based on the specified id
-						var panelIndex = this.$accordion.find('.as-panel#' + panelId).index();
-
-						if (panelIndex != -1)
-							this.openPanel(panelIndex);
-					} else {
-						this.openPanel(panelIdNumber);
-					}
-				}
-					
+				if (this.$accordion.attr('id') == accordionId)
+					return {'accordionID': accordionId, 'panelId': panelId};
 			}
+
+			return false;
+		},
+
+		_gotoHash: function(hash) {
+			var result = this._parseHash(hash);
+
+			if (result === false)
+				return;
+
+			var panelId = result.panelId,
+				panelIdNumber = parseInt(panelId, 10);
+
+			// check if the specified panel id is a number or string
+			if (isNaN(panelIdNumber)) {
+				// get the index of the panel based on the specified id
+				var panelIndex = this.$accordion.find('.as-panel#' + panelId).index();
+
+				if (panelIndex != -1)
+					this.openPanel(panelIndex);
+			} else {
+				this.openPanel(panelIdNumber);
+			}
+
 		},
 
 		destroyDeepLinking: function() {
