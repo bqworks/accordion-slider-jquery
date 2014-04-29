c63726eb77cd642eb76a6ca14624d3da5034a285
davidghi <david.ghiurau@gmail.com>
2014-04-29 21:51:00 +0300

    fixed background swapping when startPanel is used

--- dist/js/jquery.accordionSlider.js
+++ dist/js/jquery.accordionSlider.js
@@ -3613,7 +3613,7 @@ JWPlayerVideo.prototype.replay = function() {
 						that._fadeOutBackground(background);
 				}
 
-				if (event.previousIndex !== -1) {
+				if (event.previousIndex !== -1 && event.index !== event.previousIndex) {
 					// get the previously opened panel
 					var previousPanel = that.getPanelAt(event.previousIndex),
 						previousBackground = previousPanel.$panel.find('.as-background'),
