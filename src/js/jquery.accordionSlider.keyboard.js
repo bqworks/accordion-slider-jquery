46c97dc142e552d0aeffceb104c5127a0ae57d56
David Ghiurau <david.ghiurau@gmail.com>
2016-07-02 17:34:05 +0300

    prevent error when Enter is pressed on slides without links

--- src/js/jquery.accordionSlider.keyboard.js
+++ src/js/jquery.accordionSlider.keyboard.js
@@ -41,7 +41,11 @@
 					else
 						that.nextPanel();
 				} else if (event.which === 13) {
-					that.$accordion.find('.as-panel').eq(that.currentIndex).children('a')[0].click();
+					var link = that.$accordion.find('.as-panel').eq(that.currentIndex).children('a');
+
+					if ( link.length !== 0 ) {
+						link[0].click();
+					}
 				}
 			});
 		},
