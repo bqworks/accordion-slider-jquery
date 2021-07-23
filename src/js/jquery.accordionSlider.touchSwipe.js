e5dcfb63f9ff9cc20dc33615f1e1407c33c405c8
David Ghiurau <david.ghiurau@gmail.com>
2021-07-23 15:39:59 +0300

    improve touch swipe support

--- src/js/jquery.accordionSlider.touchSwipe.js
+++ src/js/jquery.accordionSlider.touchSwipe.js
@@ -49,10 +49,16 @@
 				}
 			});
 
+			// re-enable links
+			this.$panelsContainer.on( 'touchstart.' + NS, function( event ) {
+				$(this).find('[data-disabledlink]').css('pointer-events', '').removeAttr('data-disabledlink');
+			});
+
 			// prevent 'tap' events unless the panel is opened
-			this.$panelsContainer.find( 'a' ).on( 'touchstart.' + NS, function( event ) {
+			this.$panelsContainer.find( 'a' ).on( 'touchend.' + NS, function( event ) {
 				if ( $(this).parents('.as-panel').hasClass( 'as-opened' ) === false ) {
-					event.preventDefault();
+					$(this).css('pointer-events', 'none');
+					$(this).attr('data-disabledlink', 'true');
 				}
 			});
 
@@ -120,10 +126,10 @@
 			var distance = this.settings.orientation === 'horizontal' ? this.touchDistance.x : this.touchDistance.y,
 				oppositeDistance = this.settings.orientation === 'horizontal' ? this.touchDistance.y : this.touchDistance.x;
 
-			if (Math.abs(distance) > Math.abs(oppositeDistance))
-				event.preventDefault();
-			else
+			if (Math.abs(distance) <= Math.abs(oppositeDistance) || this.getTotalPages() === 1 || (this.getTotalPages() > 1 && this.currentPage === this.getTotalPages() - 1))
 				return;
+
+			event.preventDefault();
 			
 			// get the current position of panels' container
 			var currentPanelsPosition = parseInt(this.$panelsContainer.css(this.positionProperty), 10);
@@ -149,7 +155,7 @@
 			// check if there is intention for a tap
 			if (this.isTouchMoving === false || this.isTouchMoving === true && Math.abs(this.touchDistance.x) < 10 && Math.abs(this.touchDistance.y) < 10) {
 				var index = $(event.target).parents('.as-panel').index();
-
+				
 				if (typeof event.originalEvent.touches !== 'undefined' && index !== this.currentIndex && index !== -1 && this.openPanelOn !== 'never') {
 					this.openPanel(index);
 				}
